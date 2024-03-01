e266406d12df4350505812c4781263c4443938f9
Monarch <136090488+NotMonarch@users.noreply.github.com>
2024-03-01 21:38:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/NoTMonarch",
+    "https://github.com/notMonarch/yelanmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
