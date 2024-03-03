6504d1213bba2ca068d01a972707c80e1a62e802
Shivang <132191347+shhvang@users.noreply.github.com>
2024-03-03 14:50:17 +0530

    Inline Buttons Simplified

--- TGNMusic/utils/inline/start.py
+++ TGNMusic/utils/inline/start.py
@@ -8,9 +8,9 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["Recruit Me"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["Support"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
@@ -20,18 +20,16 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
-        ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+                text=_["Recruit Me"],
+                url=f"https://t.me/{app.username}?startgroup=true"),
+            InlineKeyboardButton(text=_["Commands"], callback_data="settings_back_helper")
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
+            InlineKeyboardButton(text=_["Developer"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["Support"], url=config.SUPPORT_CHAT),
         ],
+        [
+            InlineKeyboardButton(text=_["Opacity"], url=f"https://t.me/iopacity")
+        ]
     ]
     return buttons
