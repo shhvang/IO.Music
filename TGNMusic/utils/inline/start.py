42b640ffa53cede65d8b70dbedf68397683c0c8e
Shivang <132191347+shhvang@users.noreply.github.com>
2024-03-03 15:39:37 +0530

    some fixes

--- TGNMusic/utils/inline/start.py
+++ TGNMusic/utils/inline/start.py
@@ -8,9 +8,9 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["Recruit Me"], url=f"https://t.me/{app.username}?startgroup=true"
+                text="Recruit Me", url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["Support"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text="Support", url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
@@ -19,15 +19,15 @@ def start_panel(_):
 def private_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(text=_["Recruit Me"], url=f"https://t.me/{app.username}?startgroup=true"),
-            InlineKeyboardButton(text=_["Commands"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(text="Recruit Me", url=f"https://t.me/{app.username}?startgroup=true"),
+            InlineKeyboardButton(text="Commands", callback_data="settings_back_helper"),
         ],
         [
-            InlineKeyboardButton(text=_["Developer"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["Support"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text="Developer", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="Support", url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["Opacity"], url=f"https://t.me/iopacity"),
+            InlineKeyboardButton(text="Opacity", url=f"https://t.me/iopacity"),
         ]
     ]
     return buttons
