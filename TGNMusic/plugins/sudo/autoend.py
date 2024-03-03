3935db22d8af4c5eadb7a7a5b2a2a1d1086aa09c
Shivang <132191347+shhvang@users.noreply.github.com>
2024-03-03 20:37:12 +0530

    Update autoend.py

--- TGNMusic/plugins/sudo/autoend.py
+++ TGNMusic/plugins/sudo/autoend.py
@@ -8,17 +8,17 @@ from TGNMusic.utils.database import autoend_off, autoend_on
 
 @app.on_message(filters.command("autoend") & SUDOERS)
 async def auto_end_stream(_, message: Message):
-    usage = "<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/autoend [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]"
+    usage = "<b>Example :</b>\n\n/autoend [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip().lower()
     if state == "enable":
         await autoend_on()
         await message.reply_text(
-            "» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴇɴᴀʙʟᴇᴅ.\n\nᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғᴛᴇʀ ғᴇᴡ ᴍɪɴs ᴡʜᴇɴ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ."
+            "» Auto End stream enabled\n\nAssistant will automatically leave the VC when no one is listening"
         )
     elif state == "disable":
         await autoend_off()
-        await message.reply_text("» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴅɪsᴀʙʟᴇᴅ.")
+        await message.reply_text("» Auto End stream disabled")
     else:
         await message.reply_text(usage)
