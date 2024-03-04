f7302c9eae61ea8c826ded3791017f6d168b2f50
Monarch <136090488+NotMonarch@users.noreply.github.com>
2024-03-04 20:30:10 +0530

    Update call.py

--- TGNMusic/core/call.py
+++ TGNMusic/core/call.py
@@ -10,15 +10,16 @@ from pytgcalls.exceptions import (
     AlreadyJoinedError,
     NoActiveGroupCall,
 )
-from ntgcalls import TelegramServerError
-from pytgcalls.types import Update
-from pytgcalls.types.stream.legacy import AudioPiped
-from pytgcalls.types.stream.legacy import AudioVideoPiped
-from pytgcalls.types.stream.legacy.quality import Studio, FHD_1080p
-from pytgcalls.types.stream import StreamAudioEnded
 from pytgcalls import PyTgCalls
 from pytgcalls import idle
 from pytgcalls.types import MediaStream
+from ntgcalls import TelegramServerError
+from pytgcalls.types.raw import Update
+from pytgcalls.types.raw stream.legacy import AudioPiped
+from pytgcalls.types.raw stream.legacy import AudioVideoPiped
+from pytgcalls.types.stream.raw legacy.quality import Studio, FHD_1080p
+from pytgcalls.types.raw stream import StreamAudioEnded
+
 
 import config
 from TGNMusic import LOGGER, YouTube, app
