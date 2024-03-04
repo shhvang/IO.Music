1ad3576c710fde11cc4bef72401c02f60f61abcb
Shivang <132191347+shhvang@users.noreply.github.com>
2024-03-04 15:12:08 +0530

    Update call.py

--- TGNMusic/core/call.py
+++ TGNMusic/core/call.py
@@ -12,9 +12,9 @@ from pytgcalls.exceptions import (
 )
 from ntgcalls import TelegramServerError
 from pytgcalls.types import Update
-from pytgcalls.types.audiopiped import AudioPiped
-from pytgcalls.types.audiovideopiped import AudioVideoPiped
-from pytgcalls.types.raw.quality import HighQualityAudio, MediumQualityVideo
+from pytgcalls.types.stream.legacy import AudioPiped
+from pytgcalls.types.stream.legacy import AudioVideoPiped
+from pytgcalls.types.stream.legacy.quality import HighQualityAudio, MediumQualityVideo
 from pytgcalls.types.stream import StreamAudioEnded
 
 import config
