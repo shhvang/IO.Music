b4f051e02e79070b3df47f1b4b526dd8a15b3136
Shivang <132191347+shhvang@users.noreply.github.com>
2024-03-26 18:45:21 +0530

    trying to add hey io

--- IOMusic/plugins/admins/pause.py
+++ IOMusic/plugins/admins/pause.py
@@ -14,8 +14,9 @@ from config import BANNED_USERS
 async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
-    await music_off(chat_id)
-    await IO.pause_stream(chat_id)
-    await message.reply_text(
-        _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
-    )
+        if message.text.startswith("Hey io pause"):
+            await music_off(chat_id)
+            await IO.pause_stream(chat_id)
+            await message.reply_text(
+                _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
+            )
